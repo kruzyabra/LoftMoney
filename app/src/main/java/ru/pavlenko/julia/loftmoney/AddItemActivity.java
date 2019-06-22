7136413628dc56177532fb04f264521d28573fd9
Kruzyabra <kruzyabra@yandex.ru>
2019-06-22 04:24:11 +0300

    add intent for open AddItemActivity add method for changing addButton text color add colors to layout files

--- app/src/main/java/ru/pavlenko/julia/loftmoney/AddItemActivity.java
+++ app/src/main/java/ru/pavlenko/julia/loftmoney/AddItemActivity.java
@@ -2,12 +2,74 @@ package ru.pavlenko.julia.loftmoney;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    EditText titleEditText;
+    EditText priceEditText;
+    Button addButton;
+
+    String title;
+    String price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        addButton = findViewById(R.id.add_button);
+        titleEditText = findViewById(R.id.title_input);
+        priceEditText = findViewById(R.id.price_input);
+
+        titleEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                title = s.toString();
+                changeButtonTextColor();
+            }
+        });
+
+        priceEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                price = s.toString();
+                changeButtonTextColor();
+            }
+        });
+
+    }
+
+    private void changeButtonTextColor() {
+        if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(price)) {
+            addButton.setTextColor(getResources().getColor(R.color.active_add_button_color));
+        } else
+        {
+            addButton.setTextColor(getResources().getColor(R.color.hint_color));
+        }
     }
 }

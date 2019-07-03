b5bb5bf96452c520d66cb76c355967f7196e1407
Kruzyabra <kruzyabra@yandex.ru>
2019-07-03 11:52:10 +0300

    Add ViewPager.

--- app/src/main/java/ru/pavlenko/julia/loftmoney/BudgetActivity.java
+++ app/src/main/java/ru/pavlenko/julia/loftmoney/BudgetActivity.java
@@ -1,20 +1,48 @@
 package ru.pavlenko.julia.loftmoney;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BudgetActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment_container, BudgetFragment.newInstance());
-        fragmentTransaction.commit();
+        ViewPagerAdapter mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = findViewById(R.id.view_pager);
+
+        mViewPager.setAdapter(mViewPagerAdapter);
+
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                case 0:
+                    return BudgetFragment.newInstance();
+                case 1:
+                    return BudgetFragment.newInstance();
+            }
+            return null;
+        }
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
     }
 }
--- app/src/main/java/ru/pavlenko/julia/loftmoney/BudgetFragment.java
+++ app/src/main/java/ru/pavlenko/julia/loftmoney/BudgetFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
